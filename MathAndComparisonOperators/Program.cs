0d82ce0f51ec0ecd3bda51505e36540c9f21804c
Henry Lee <63754132+Henry-William-Lee@users.noreply.github.com>
2022-01-28 20:15:52 -0500

    added files

---
+++ MathAndComparisonOperators/Program.cs
@@ -0,0 +1,48 @@
+﻿ using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 34;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+
+            //            int difference = 46 - 12;
+            //            Console.WriteLine(difference);
+            //            Console.ReadLine();
+
+            //             int product = 46 * 12;
+            //            Console.WriteLine(product);
+            //            Console.ReadLine();
+
+            //             int quotient = 46 / 12;
+            //            Console.WriteLine(quotient);
+            //            Console.ReadLine();
+
+            //            int remainder = 11 % 4;
+            //            Console.WriteLine(remainder);
+            //            Console.ReadLine();
+
+            //bool trueOrFalse = 14 > 7;
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            int height = 6;
+            int heightwish = 7;
+            bool isTaller = height != heightwish;
+
+            Console.WriteLine(isTaller);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ MathandComparisonOperatorSubmissionAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MathandComparisonOperatorSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program!");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Enter Hourly Rate:");
+            int hrlyRate1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours Worked Per Week: ");
+            int hrsPerWk1 = Convert.ToInt32(Console.ReadLine());
+            int ttlmny1 = hrlyRate1 * hrsPerWk1;
+            int annualSal1 = ttlmny1 * 52;
+            Console.WriteLine("Annual Salary of Person 1 is; " + annualSal1);
+
+            //Person 2
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Enter Hourly Rate:");
+            int hrlyRate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours Worked Per Week: ");
+            int hrsPerWk2 = Convert.ToInt32(Console.ReadLine());
+            int ttlmny2 = hrlyRate2 * hrsPerWk2;
+            int annualSal2 = ttlmny2 * 52;
+            Console.WriteLine("Annual salary of Person 2: " + annualSal2);
+
+            bool makemore = annualSal1 > annualSal2;
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?" + makemore);
+
+
+
+
+
+
+
+
+        }
+    }
+}
