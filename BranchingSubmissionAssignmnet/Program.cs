193a5f30609e1a35c915c5870b8c3bdfb49561a7
Henry Lee <63754132+Henry-William-Lee@users.noreply.github.com>
2022-02-01 00:24:00 -0500

    Branching Submission Assignment.

---
+++ BranchingSubmissionAssignmnet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BranchingSubmissionAssignmnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to package express!");
+
+            // ask for the measurements of the package and convert them to data to be used. 
+
+            Console.WriteLine("What is the weight of your package?");
+
+            int pkgWht = Convert.ToInt32(Console.ReadLine());
+
+            if (pkgWht >50)
+            {
+                Console.Write("Your package is too heavy. ");
+
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("What is the height of your package?");
+            int pkgHt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the length of your package?");
+            int pkgLen = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the width of your package?");
+            int pkgWdth= Convert.ToInt32(Console.ReadLine());
+
+            int pckgttl = pkgWdth + pkgHt + pkgHt;
+
+
+            if (pckgttl > 50) 
+            {
+                Console.WriteLine("Your package is too large.");
+            }
+
+            int pckttl2 = pkgHt * pkgLen * pkgWdth;
+
+            // pkg ttl 2 * pkg weight
+            int pckttl3 = pckttl2 * pkgWht;
+
+            //package weight * pkg height * pck width * pkg length / 100
+            decimal pckttl4 = Convert.ToDecimal(pckttl3);
+            Console.WriteLine("Your total is $" + pckttl4 + ".");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
