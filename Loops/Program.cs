786d4d4170dfb66463cece033b07e6877e9ca0a7
Henry Lee <63754132+Henry-William-Lee@users.noreply.github.com>
2022-02-01 21:20:47 -0500

    Added Loops project.

---
+++ Loops/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your password?");
+            string password = Console.ReadLine();
+            bool isGuessed = password == "Password";
+
+            do
+            {
+                switch (password)
+                {
+
+                    case "Fido":
+                        Console.WriteLine("You guessed Fido. Try again.");
+                        Console.WriteLine("Guess your password.");
+                        password = Console.ReadLine();
+                        break;
+
+
+                    case "Cujo":
+                        Console.WriteLine("You guessed Cujo. Try again.");
+                        Console.WriteLine("Guess your password.");
+                        password = Console.ReadLine();
+                        break;
+
+                    case "Scooby Doo":
+                        Console.WriteLine("You guessed Scooby Doo. Try again.");
+                        Console.WriteLine("Guess your password.");
+                        password = Console.ReadLine();
+                        break;
+
+
+                    case "Password":
+                        Console.WriteLine("You guessed \"Password\". That is correct.");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Try again.");
+                        Console.WriteLine("Guess your password");
+                        password = Console.ReadLine();
+                        break;
+
+                }    
+            }
+            while (!isGuessed);
+         
+
+
+        }
+    }
+}
