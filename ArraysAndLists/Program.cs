b90f36abf23d56174800049ccf45a64e4fbc5c6f
Henry Lee <63754132+Henry-William-Lee@users.noreply.github.com>
2022-02-03 23:46:18 -0500

    Fixed this.

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -8,14 +8,7 @@ namespace ArraysAndLists
         static void Main()
         {
 
-            List<string> intList = new List<string>();
-            intList.Add("G'Day");
-            intList.Add("Mate");
-            intList.Remove("Mate");
-
-            Console.WriteLine(intList[0]);
-            Console.ReadLine();
-
+           //integer array
             int[] numArray = new int[5];
             numArray[0] = 6;
             numArray[1] = 21;
@@ -23,18 +16,40 @@ namespace ArraysAndLists
             numArray[3] = 300;
             numArray[4] = 50000;
            
-            Console.WriteLine("Pick a number from the list: \n 6 \n 21 \n 42 \n 300 \n 50000. ");
+            //function used to pick the number in the array. 
+            Console.WriteLine("Pick an index between 0 and 4.");
             int answer = Convert.ToInt32(Console.ReadLine());
 
-            if (answer != numArray[0] || answer != numArray[1] || answer != numArray[2] || answer != numArray[3] || answer != numArray[4])
+            if (answer >= 0 && answer <= 4)
             {
-                Console.WriteLine("You did not pick the right number!");
+                Console.WriteLine("item chosen at index " + numArray[answer]);
             }
-            else (answer == numArray[0] || answer == numArray[1] || answer == numArray[2] || answer == numArray[3] || answer == numArray[4])
+            else 
             {
                 Console.WriteLine("You did not pick the right number!");
             }
 
+
+            List<string> intList = new List<string>();
+            intList.Add("G'Day");
+            intList.Add("Mate");
+            intList.Add("How");
+            intList.Add("Are");
+            intList.Add("You");
+            intList.Add("Today?");
+
+
+            Console.WriteLine("Pick an index between 0 and 5.");
+            int answers = Convert.ToInt32 (Console.ReadLine());
+
+            if (answers >= 0 && answers <= 5)
+            {
+                Console.WriteLine("item chosen at index = " + intList[answers]);
+            }
+            else
+            {
+                Console.WriteLine("You did not pick the right number!");
+            }
 
             Console.ReadLine();
         }
